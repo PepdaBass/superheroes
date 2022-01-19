0cbc3cb46835cf69e0d3e33a7a8abc38c00e0364
Jeffrey Peplinski <jeffreypeplinski@jeffreys-mbp.lan>
2022-01-19 15:31:18 -0500

    Added superhero database to mysql workbench through django. Migrated everything over.

--- superheroes/superheroes/settings.py
+++ superheroes/superheroes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'superhero',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'superheroes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'superheroes_database',
+        'USER': 'root',
+        'PASSWORD': 'OllKorrect15!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True
+        }
     }
 }
 
