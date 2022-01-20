0caaf82d5aed91d34baf3e6110354e8fb6d7cb4e
Jeffrey Peplinski <jeffreypeplinski@jeffreys-mbp.lan>
2022-01-20 12:31:49 -0500

    Added detail.html fileand reviewed the django start up steps.

--- superheroes/superhero/urls.py
+++ superheroes/superhero/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'superhero'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:hero_id>/', views.detail, name='detail')
 ]
--- superheroes/superhero/views.py
+++ superheroes/superhero/views.py
@@ -9,3 +9,10 @@ def index(request):
         'all_heroes' : all_heroes
     }
     return render(request, 'superhero/index.html', context)
+
+def detail(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
+    context = {
+        'single_hero' : single_hero
+    }
+    return(request, 'superhero/detail.html', context)
