8bf0dc9fcb44326b84916b109df57fd10854bbed
Jeffrey Peplinski <jeffreypeplinski@jeffreys-mbp.lan>
2022-01-20 14:24:00 -0500

    Got the detail.html page to display relevant details.

--- superheroes/superhero/views.py
+++ superheroes/superhero/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Superhero
 
+
 # Create your views here.
 def index(request):
     all_heroes = Superhero.objects.all()
@@ -15,4 +16,4 @@ def detail(request, hero_id):
     context = {
         'single_hero' : single_hero
     }
-    return(request, 'superhero/detail.html', context)
+    return render(request, 'superhero/detail.html', context)
