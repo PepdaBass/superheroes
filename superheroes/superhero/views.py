a32c32245a1941a11e25c2b881ed51c50b47a097
Jeffrey Peplinski <jeffreypeplinski@jeffreys-mbp.lan>
2022-01-27 23:45:14 -0500

    Fixed display and sizing of superhero image and fixed edit() function so that it works and will autofill details which were previously entered.

--- superheroes/superhero/views.py
+++ superheroes/superhero/views.py
@@ -38,17 +38,16 @@ def delete(request, hero_id):
     return HttpResponseRedirect(reverse('superhero:index'))
 
 def edit(request, hero_id):
+    single_hero = Superhero.objects.get(pk=hero_id)
     if request.method == "POST":
-        name = request.POST.get('name')
-        alter_ego = request.POST.get('alter_ego')
-        primary = request.POST.get('primary')
-        secondary = request.POST.get('secondary')
-        catchphrase = request.POST.get('catchphrase')
-        single_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catchphrase=catchphrase)
+        single_hero.name = request.POST.get('name')
+        single_hero.alter_ego = request.POST.get('alter_ego')
+        single_hero.primary = request.POST.get('primary')
+        single_hero.secondary = request.POST.get('secondary')
+        single_hero.catchphrase = request.POST.get('catchphrase')
         single_hero.save()
         return HttpResponseRedirect(reverse('superhero:index'))
     else:
-        single_hero = Superhero.objects.get(pk=hero_id)
         context = {
         'single_hero' : single_hero
     }
